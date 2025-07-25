6179b37224e316a7b0b3dee1b9c70d1b937cf1ba
sarayounan <sarahfouad64@gmail.com>
2025-07-25 20:01:17 +0300

    favorite screen without model cause iam wating nour updates

---
+++ Flutter/zawal1-main/lib/screens/favorite_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:zawal/constants/app_colors.dart';
+import 'package:zawal/constants/app_textstyles.dart';
+import 'package:zawal/routes/app_routes.dart';
+
+class FavoriteScreen extends StatefulWidget {
+  const FavoriteScreen({super.key});
+
+  @override
+  State<FavoriteScreen> createState() => _FavoriteScreenState();
+}
+
+class _FavoriteScreenState extends State<FavoriteScreen> {
+  List<dynamic> favoriteItems = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, AppRoutes.profile);
+          },
+          icon: Icon(Icons.arrow_back, color: AppColors.primary),
+        ),
+        title: Text('Edit profile', style: AppTextStyles.heading),
+        centerTitle: true,
+        backgroundColor: AppColors.background,
+      ),
+      body:
+          favoriteItems.isEmpty
+              ? const Center(child: Text('No favorite yet '))
+              : ListView.builder(
+                itemCount: favoriteItems.length,
+                itemBuilder: (context, index) {
+                  //i will show favorite items here
+                },
+              ),
+    );
+  }
+}
